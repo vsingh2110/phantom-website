0051db3fd8e107ab17f7f2d8859552a95a94ac59
Vikas Singh <Vikas@olibr.com>
2025-01-03 13:13:20 +0530

    popup boxes

--- js_files/main.js
+++ js_files/main.js
@@ -608,3 +608,41 @@ var swiper = new Swiper(".slide-content", {
       },
   },
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// ----polyfill popup loaded-google docs---
+    const dialog = document.querySelector('dialog');
+    dialogPolyfill.registerDialog(dialog);
+
+
+
+
+
+// -----popup loaded---goggle docs---
+  document.addEventListener("DOMContentLoaded", function () {
+      const modal = document.getElementById("modal");
+      const openButton = document.querySelector(".open-button");
+      const closeButton = document.querySelector(".close-button");
+
+      if (openButton) {
+          openButton.addEventListener("click", () => {
+              modal.showModal();
+          });
+      }
+
+      if (closeButton) {
+          closeButton.addEventListener("click", () => {
+              modal.close();
+          });
+      }
+  });
